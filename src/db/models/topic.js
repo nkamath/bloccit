6bd29548b5f17ba60fabd7049dec3aec5d47b29b
Nritya Kamath <nritya.k@gmail.com>
2019-08-04 11:05:53 -0700

    Post data creation, need to migrate data

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+       type: DataTypes.STRING,
+       allowNull: false
+     },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
@@ -10,6 +16,10 @@ module.exports = (sequelize, DataTypes) => {
      foreignKey: "topicId",
      as: "Banners",
    });
+   Topic.hasMany(models.Post, {
+       foreignKey: "topicId",
+       as: "posts"
+    });
   };
   return Topic;
 };
